5e6ba25e9de37ee6e92d6d7b7fa72efdb6f3b080
Tuan <tnguy258@ford.com>
2024-04-12 08:04:40 +0700

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading-bar";
+import NewTweet from "./NewTweet";
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+  return (
+    <div>
+      <LoadingBar />
+      {props.loading === true ? null : <NewTweet />}
+    </div>
+  );
+};
+
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
+
+export default connect(mapStateToProps)(App);
